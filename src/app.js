af81b6468b3011dcda9c18a03f96488f26951d06
Shin-sibainu <shincode0712@gmail.com>
2023-09-05 11:18:19 +0900

    mod

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 // console.log('Hello World');
 
-const http = require('http');
+const http = require('http')
 
-const name = 'world';
+const name = 'world'
 // const sample = 'Hello_Wolrd';
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.write('Hello, ' + name + '!');
-    res.end();
-    return;
+    res.write('Hello, ' + name + '!')
+    res.end()
+    return
   }
 
-  res.write('This is a another endpoint');
-  res.end();
-});
-server.listen(3000);
-console.log('Server running on port 3000');
+  res.write('This is a another endpoint')
+  res.end()
+})
+server.listen(3000)
+console.log('Server running on port 3000')
